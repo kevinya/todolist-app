ec995b58ef017a4fa17e32672ca006fd779e73fa
Kevin Ya <ya.kevin.t@gmail.com>
2013-06-12 03:21:09 +0200

    ajout de l'ajout de tache

--- src/main/java/fr/kevinya/todolistapp/activity/MainActivity.java
+++ src/main/java/fr/kevinya/todolistapp/activity/MainActivity.java
@@ -1,18 +1,15 @@
 package fr.kevinya.todolistapp.activity;
 
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.message.BasicNameValuePair;
-
 import android.app.ListActivity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import fr.kevinya.todolistapp.R;
@@ -36,23 +33,38 @@ public class MainActivity extends ListActivity {
 		taskList = new ArrayList<Task>();
 		taskAdapter = new TaskAdapter(this, taskList);
 		setListAdapter(taskAdapter);
+		
+		getListView().setOnItemClickListener(new OnItemClickListener() {
 
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				Task task = (Task) taskAdapter.getItem((int) arg3);
+				Integer id = task.getId();
+				String name = task.getName();
+				Integer status = (task.getStatus() == 0) ? 1 : 0;
+				RequestTasks requestTask = new RequestTasks("get", getString(R.string.url_server) + "/tasks/update/" + id + "/" + name + "/" + status, null, null, new GetTasksCallback() {
+					
+					@SuppressWarnings("unchecked")
+					@Override
+					public void onDataReceived(Object data) {
+						taskList = (List<Task>) data;
+						taskAdapter.setTaskList(taskList);
+						taskAdapter.notifyDataSetChanged();
+					}
+				});
+				requestTask.execute(null, null);
+			}
+		});
+		
 		editText = (EditText) findViewById(R.id.editText1);
 		button = (Button) findViewById(R.id.button1);
 		button.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-			    nameValuePairs.add(new BasicNameValuePair("name", editText.getText().toString())); 
-			    nameValuePairs.add(new BasicNameValuePair("status", "0"));
-			    HttpEntity httpEntity = null;
-				try {
-					httpEntity = new UrlEncodedFormEntity(nameValuePairs);
-				} catch (UnsupportedEncodingException e) {
-					e.printStackTrace();
-				}
-				RequestTasks requestTask = new RequestTasks("post", getString(R.string.url_server) + "/tasks", httpEntity, null, null);
+				String taskName = Uri.encode(editText.getText().toString());
+				RequestTasks requestTask = new RequestTasks("get", getString(R.string.url_server) + "/tasks/add/" + taskName, null, null, null);
 				requestTask.execute(null, null);
 				editText.setText("");
 				refreshList();
